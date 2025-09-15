e0e5dd92b7d8ebb0cc67c1bc037b88ae16ad32e2
Menna Elsba3i <elsbaayemenna@gmail.com>
2025-09-15 04:18:54 +0300

    Some Updates Was addes

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Quiz-App
-Simple quiz app with js
+# Quiz Application
+
+A simple quiz application that allows users to answer multiple-choice questions and see their final score.
+
+---
+
+##  Features
+- Display multiple-choice questions.
+- Fetch questions and answers from a JSON file.
+- Check if the selected answer is correct.
+- Show the final score at the end.
+- Simple and user-friendly interface.
+
+---
+
+##  Technologies Used
+- **HTML**  
+- **CSS**  
+- **JavaScript** (used for quiz logic, interactivity, and fetching data from JSON file)
+
+---
+
+##  How to Run
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/MennaHamadaElsba3i/Quiz-App
--- app.js
+++ app.js
@@ -4,9 +4,13 @@ let mainbulletscontainer = document.querySelector('.bullets .spans');
 let quezErea = document.querySelector('.quiz-area');
 let ansarea = document.querySelector('.answers-area');
 let supbutton = document.querySelector('.submit-button');
+let resContainer = document.querySelector('.results');
+let coundownelment = document.querySelector('.countdown');
 
 let currentIndex = 0;
 let rightAns = 0;
+let counterinterval;
+
 function getques() {
      let myreq = new XMLHttpRequest();
      myreq.onreadystatechange = function () {  // لما حالة الريكويست تتغير هنبدا نتشيك على الريدي ستيت اللي هي حالة الريكويست 
@@ -18,14 +22,18 @@ function getques() {
                let quescount = quesobjs.length;
                createpullets(quescount);
                addquesdata(quesobjs[currentIndex], quescount);
-               supbutton, onclick = () => {
-                    let rightAns = quesobjs[currentIndex].right_answer;
-                    //نزود الاندكس
+               countdown(60, quescount);
+               supbutton.onclick = () => {
+                    let theRightAnswer = quesobjs[currentIndex].right_answer;
                     currentIndex++;
-                    checkAns(rightAns, quescount);
+                    checkAns(theRightAnswer, quescount);
                     quezErea.innerHTML = "";
                     ansarea.innerHTML = "";
                     addquesdata(quesobjs[currentIndex], quescount);
+                    handlebullets();
+                    clearInterval(counterinterval)
+                    countdown(60, quescount);
+                    showResults(quescount);
                }
           }
      }
@@ -47,35 +55,38 @@ function createpullets(num) {
 }
 
 function addquesdata(obj, count) {
-     // اول حاجه هنكريت السؤال
-     let quesTitle = document.createElement("h2");
-     let quesText = document.createTextNode(obj['title']);
-     // let quesTExt = document.createTextNode(obj.title);
-     quesTitle.appendChild(quesText);
-     quezErea.appendChild(quesTitle);
+     if (currentIndex < count) {
+          // اول حاجه هنكريت السؤال
+          let quesTitle = document.createElement("h2");
+          let quesText = document.createTextNode(obj['title']);
+          // let quesTExt = document.createTextNode(obj.title);
+          quesTitle.appendChild(quesText);
+          quezErea.appendChild(quesTitle);
 
-     for (let i = 1; i <= 4; i++) {
-          let mainansdiv = document.createElement('div');
-          mainansdiv.className = 'answer'
+          for (let i = 1; i <= 4; i++) {
+               let mainansdiv = document.createElement('div');
+               mainansdiv.className = 'answer'
 
-          let radioAns = document.createElement('input');
-          radioAns.name = "ques"
-          radioAns.type = "radio";
-          radioAns.id = `answer_${i}`;
-          radioAns.dataset.answer = obj[`answer_${i}`];
+               let radioAns = document.createElement('input');
+               radioAns.name = "ques"
+               radioAns.type = "radio";
+               radioAns.id = `answer_${i}`;
+               radioAns.dataset.answer = obj[`answer_${i}`];
 
-          let thelabel = document.createElement('label');
-          thelabel.htmlFor = `answer_${i}`;
-          let lebelText = document.createTextNode(obj[`answer_${i}`]);
-          if (i === 1) {
-               radioAns.checked = true;
+               if (i === 1) {
+                    radioAns.checked = true;
+               }
+
+               let thelabel = document.createElement('label');
+               thelabel.htmlFor = `answer_${i}`;
+               let lebelText = document.createTextNode(obj[`answer_${i}`]);
+
+
+               thelabel.appendChild(lebelText);
+               mainansdiv.appendChild(radioAns);
+               mainansdiv.appendChild(thelabel);
+               ansarea.appendChild(mainansdiv);
           }
-
-          thelabel.appendChild(lebelText);
-
-          mainansdiv.appendChild(radioAns);
-          mainansdiv.appendChild(thelabel);
-          ansarea.appendChild(mainansdiv);
      }
 }
 
@@ -89,5 +100,56 @@ function checkAns(rans, count) {
      }
      if (rans === chosenans) {
           rightAns++;
+     }
+}
+
+function handlebullets() {
+     let bulspan = document.querySelectorAll('.bullets .spans span');
+     let arrayofspan = Array.from(bulspan);
+     arrayofspan.forEach((span, index) => {
+          if (currentIndex === index) {
+               span.classList.add("on");
+          }
+     })
+}
+
+function showResults(count) {
+     let thereslts;
+     if (currentIndex === count) {  // هنا معناه ان الاسئلة خلصت ف اظهرلي النتايج
+          quezErea.remove();
+          ansarea.remove();
+          supbutton.remove();
+          mainbullets.remove();
+
+          if (rightAns > (count / 2) && rightAns < count) {
+               thereslts = `<span class="good">Good</span> ${rightAns} from ${count}Is Good`
+          }
+          else if (rightAns === count) {
+               thereslts = `<span class="perfect">perfect</span> All Answers Is Good`
+          }
+          else {
+               thereslts = `<span class="bad">bad</span> ${rightAns} from ${count}`
+          }
+          resContainer.innerHTML = thereslts;
+     }
+}
+
+function countdown(duaration, count) {
+     if (currentIndex < count) {
+          let mins, secs;
+          counterinterval = setInterval(() => {
+               mins = parseInt(duaration / 60); //هو دلوقتي ممكن يديني 150 ثانية لما بنقسمها على الستي مش بتقبل وبتطلع عدد صحيح طيب لو مطلعتش عدد صحيح هاخد العشري ده هو الثواني
+               secs = parseInt(duaration % 60);
+
+               mins = mins < 10 ? `0${mins}` : mins;  // 00:00
+               secs = mins < 10 ? `0${secs}` : secs;
+
+               coundownelment.innerHTML = `${mins}:${secs}`;
+
+               if (--duaration < 0) {
+                    clearInterval(counterinterval);
+                    supbutton.click();
+               }
+          }, 1000);
      }
 }
